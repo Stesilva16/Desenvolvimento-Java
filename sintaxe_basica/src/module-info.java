0afd68061089312941476d7ac527bac95347f553
Stesilva16 <stefany.silva2020.16@gmail.com>
2023-01-31 12:16:30 -0300

    Initial commit

---
+++ sintaxe_basica/src/module-info.java
@@ -0,0 +1,2 @@
+module sintaxe_basica {
+}
---
+++ sintaxe_basica/src/sintaxe_basica/Programa.java
@@ -0,0 +1,10 @@
+package sintaxe_basica;
+
+public class Programa {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("olá mundo!");
+	}
+
+}
