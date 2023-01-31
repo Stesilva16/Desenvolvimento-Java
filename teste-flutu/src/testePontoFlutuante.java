fdb3a2767af7ce59a2637789b32a455f0f96a0e3
Stesilva16 <stefany.silva2020.16@gmail.com>
2023-01-31 12:16:54 -0300

    Initial commit

---
+++ teste-flutu/src/testePontoFlutuante.java
@@ -0,0 +1,11 @@
+
+public class testePontoFlutuante {
+	
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("O salario é " + salario);
+		
+	}
+
+}
